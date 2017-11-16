fc6108eec750b01f70849c2e26261298f8d7e2ae
Ayden Rennaker <me@aydenrennaker.com>
2017-11-16 15:23:11 -0700

    dir change

--- routes/stats.js
+++ routes/stats.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var config = require('./config');
-var Query = require('./Query');
+var config = require('../config');
+var Query = require('../Query');
 
 /* GET users listing. */
 router.get('/:algo', function(req, res, next) {
