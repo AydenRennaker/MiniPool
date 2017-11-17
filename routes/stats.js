57de7f5e2349a9e240dc1b66036a11c3f4e43ac3
atanner <ayden.rennaker@gmail.com>
2017-11-16 23:40:42 -0700

    t

--- routes/stats.js
+++ routes/stats.js
@@ -6,18 +6,11 @@ var Query = require('../Query');
 var mongoose = require('mongoose');
 var Worker = mongoose.model('Worker');
 
-router.get('/workers', function(req, res, next) {
-  Worker.find(function(err, workers){
-    if(err){ return next(err); }
-    res.json(workers);
-  });
-});
-
 /* GET users listing. */
 router.get('/:algo', function(req, res, next) {
   Worker.find({algo: req.params.algo}.function(err, workers){
     if(err){ return next(err); }
-    res.render('index', { title: req.params.algo, workers: workers});
+    res.render('workers', { title: req.params.algo, workers: workers});
   });
 
 });
