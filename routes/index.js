a39fa53da3290cd29acf6e621d2f51da435c908d
Ayden Rennaker <me@aydenrennaker.com>
2017-11-27 13:50:43 -0700

    t

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/alloc', function(req, res, next) {
         console.log(key, name, speeds)
 
         //console.log(key, name, groups[key][name].length);
-        if(!result[key].length) {
+        if(result[key] == null) {
           result[key] = [];
         } else {
           result[key].push({workerName: name, totalShare: speeds, algorithm: key});
