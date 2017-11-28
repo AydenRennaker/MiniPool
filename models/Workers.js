df6a46a7495cfea3a265ce97e5eb62c6c0198c5c
Ayden Rennaker <me@aydenrennaker.com>
2017-11-28 13:13:42 -0700

    added paid var

--- models/Workers.js
+++ models/Workers.js
@@ -5,6 +5,7 @@ var WorkerSchema = new mongoose.Schema({
   speed: { type: Number, default: 0 },
   algo: { type: Number, default: 0 },
   utc: { type: Number, default: 0 },
+  paid: {type: Boolean, default: false},
   updated: { type: Date, default: Date.now },
 });
 mongoose.model('Worker', WorkerSchema);
