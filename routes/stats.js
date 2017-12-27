0b28cadef19321fcd7ab84a7bda5d3642da31378
atanner <ayden.rennaker@gmail.com>
2017-12-26 18:49:16 -0700

    test

--- routes/stats.js
+++ routes/stats.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var config = require('../config');
 var Query = require('../Query');
+var _ = require('lodash');
 
 var mongoose = require('mongoose');
 var Worker = mongoose.model('Worker');
@@ -13,6 +14,21 @@ router.get('/EtherMineStats', function(req, res, next) {
   EtherMineStats.find(function(err, stats){
     if(err){ return next(err); }
     res.json(stats);
+  });
+
+});
+
+router.get('/minDay', function(req, res, next) {
+  EtherMineStats.find(function(err, stats){
+    if(err){ return next(err); }
+    let min = _.minBy(stats, function(s){
+      return s.coinsPerMin / s.currentHashrate;
+    });
+
+
+
+
+    res.json(min);
   });
 
 });
